d83b41ddd82ac0131b4508ca5d0d95c4b22657f7
Sanija <sanijasokirka@gmail.com>
2024-04-08 09:59:58 +0300

    hh

--- qwerty.py
+++ qwerty.py
@@ -1,2 +1,24 @@
 import sqlite3
 # pieslēdzamies datu bāzze vai izveidojam datu bāzes failu
+conn=sqlite3.connect("pavasaris.db")
+
+# izveidojam kursoru....c- objekts pieslēgumā
+c=conn.cursor #rindinu pec rindinas par bauda savienojumu ar datu bāzi
+
+
+
+# izveidot tabulu
+
+c.execute('''CREATE TABLE IF NOT EXISTS tabula2(
+          id INTEGER PRIMARY KEY, 
+          krasa TEXT.
+          pilseta TEXT 
+) ''') # tākā jāizpilda komandas kas  nav pitona- tam nolukam izmantojam trīs ķepiņas
+
+
+
+
+
+#saglabāt izmaiņas....aizvērt savienojumu
+conn.commit()
+conn.close()
